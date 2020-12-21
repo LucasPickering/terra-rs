b03ab1b0977bc766e14878710d3a3d1fd800dc96
LucasPickering <lucas@lucaspickering.me>
2020-12-21 21:36:55 +0000

    Deploying to gh-pages from  @ df3c0c68e8697599e70ca0038713be93d4495b2b 🚀

---
+++ src_world_json.bundle.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkterra"] = self["webpackChunkterra"] || []).push([["src_world_json"],{
+
+/***/ "./src/world.json":
+/*!************************!*
+  !*** ./src/world.json ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = JSON.parse(\"{\\\"seed\\\":877197321,\\\"tile_radius\\\":300,\\\"elevation\\\":{\\\"octaves\\\":3,\\\"frequency\\\":1.5,\\\"lacunarity\\\":3,\\\"persistence\\\":0.3,\\\"exponent\\\":1},\\\"humidity\\\":{\\\"octaves\\\":3,\\\"frequency\\\":2,\\\"lacunarity\\\":2,\\\"persistence\\\":0.25,\\\"exponent\\\":1}}\");\n\n//# sourceURL=webpack://terra/./src/world.json?");
+
+/***/ })
+
+}]);
