3d2136e0482014c7a4928a6558aed59cfc367411
LucasPickering <lucas.pickering@markforged.com>
2021-01-09 20:54:29 +0000

    Deploying to gh-pages from  @ 23b529ed2b8ebffc8846943ff30a98df0b778247 🚀

--- main.bundle.js
+++ main.bundle.js
@@ -687,7 +687,7 @@ eval("module.exports = (async () => {\n__webpack_require__.r(__webpack_exports__
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"crates_wasm_pkg_terra-wasm_js":{"../crates/wasm/pkg/terra-wasm_bg.wasm":"fb0f593a54f910ba36e4"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"crates_wasm_pkg_terra-wasm_js":{"../crates/wasm/pkg/terra-wasm_bg.wasm":"55c232c7a043531515d7"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
