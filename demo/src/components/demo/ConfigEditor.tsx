ff90386abb3742179cdb57d8fa765699dce167aa
Lucas Pickering <lucas@lucaspickering.me>
2021-05-31 12:45:27 -0400

    Move buttons to top of config editor

--- demo/src/components/demo/ConfigEditor.tsx
+++ demo/src/components/demo/ConfigEditor.tsx
@@ -61,6 +61,25 @@ const ConfigEditor: React.FC<{ inline?: boolean }> = ({ inline = false }) => {
           <Typography variant="h2">Configure World</Typography>
         </Grid>
 
+        <Grid item xs={12} container spacing={1} justify="flex-end">
+          <Grid item {...buttonSize}>
+            <Button fullWidth variant="outlined" onClick={resetConfig}>
+              Reset to Default
+            </Button>
+          </Grid>
+          <Grid item {...buttonSize}>
+            <Button
+              disabled={!generateWorldEnabled}
+              fullWidth
+              type="submit"
+              color="primary"
+              variant="contained"
+            >
+              {inline ? "Regenerate World" : "Generate World"}
+            </Button>
+          </Grid>
+        </Grid>
+
         <Grid item {...sectionSize}>
           <ConfigSection title="General" description="General world settings">
             <ConfigInput
@@ -238,25 +257,6 @@ const ConfigEditor: React.FC<{ inline?: boolean }> = ({ inline = false }) => {
               />
             </ConfigInput>
           </ConfigSection>
-        </Grid>
-
-        <Grid item xs={12} container spacing={1} justify="flex-end">
-          <Grid item {...buttonSize}>
-            <Button fullWidth variant="outlined" onClick={resetConfig}>
-              Reset to Default
-            </Button>
-          </Grid>
-          <Grid item {...buttonSize}>
-            <Button
-              disabled={!generateWorldEnabled}
-              fullWidth
-              type="submit"
-              color="primary"
-              variant="contained"
-            >
-              {inline ? "Regenerate World" : "Generate World"}
-            </Button>
-          </Grid>
         </Grid>
       </Grid>
     </form>
