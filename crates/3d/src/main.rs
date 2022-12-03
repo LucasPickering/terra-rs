fa3af1c3e788504723f4486c0499af2766c0c7cf
Lucas Pickering <lucas@lucaspickering.me>
2022-12-03 15:43:06 -0500

    Add pan-orbit camera

--- crates/3d/src/main.rs
+++ crates/3d/src/main.rs
@@ -1,10 +1,10 @@
+mod camera;
 mod world;
 
-use crate::world::WorldPlugin;
+use crate::{camera::CameraPlugin, world::WorldPlugin};
 use bevy::{
     prelude::{
-        default, App, Camera3dBundle, Commands, PointLight, PointLightBundle,
-        Transform, Vec3,
+        default, App, Commands, PointLight, PointLightBundle, Transform,
     },
     DefaultPlugins,
 };
@@ -13,6 +13,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldPlugin)
+        .add_plugin(CameraPlugin)
         .add_startup_system(setup)
         .run();
 }
@@ -22,17 +23,11 @@ fn setup(mut commands: Commands) {
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 1500.0,
+            intensity: 15000.0,
             shadows_enabled: true,
             ..default()
         },
         transform: Transform::from_xyz(0.0, 250.0, 0.0),
-        ..default()
-    });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(200.0, 250.0, -200.0)
-            .looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
