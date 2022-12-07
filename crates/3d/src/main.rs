580c3cecf704ec6ca24d0bcafde49d22afa3a191
Lucas Pickering <lucas@lucaspickering.me>
2022-12-06 21:04:51 -0500

    Support compiling bevy to wasm

--- crates/3d/src/main.rs
+++ crates/3d/src/main.rs
@@ -4,15 +4,23 @@ mod world;
 use crate::{camera::CameraPlugin, world::WorldPlugin};
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    log::LogPlugin,
     prelude::{
-        default, App, Commands, PointLight, PointLightBundle, Transform,
+        default, App, Commands, PluginGroup, PointLight, PointLightBundle,
+        Transform,
     },
     DefaultPlugins,
 };
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            filter: "debug,wgpu_core=warn,wgpu_hal=warn".into(),
+            level: bevy::log::Level::DEBUG,
+        }))
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(WorldPlugin)
