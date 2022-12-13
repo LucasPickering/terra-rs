d6404f1651413e264c56635e8b59ac95b79a8182
Lucas Pickering <lucas@lucaspickering.me>
2022-12-13 15:53:01 -0500

    Load config from assets

--- crates/3d/src/world.rs
+++ crates/3d/src/world.rs
@@ -1,11 +1,13 @@
 use bevy::{
     prelude::{
-        debug, default, info, shape, Assets, Bundle, Color, Commands,
-        DirectionalLight, DirectionalLightBundle, Mesh, PbrBundle, Plugin, Res,
-        ResMut, StandardMaterial, Transform, Vec3,
+        default, info, shape, Added, AssetEvent, AssetServer, Assets, Color,
+        Commands, DirectionalLight, DirectionalLightBundle, Entity,
+        EventReader, Handle, IntoSystemDescriptor, Mesh, PbrBundle, Plugin,
+        Query, Res, ResMut, Resource, StandardMaterial, Transform, Vec3, With,
     },
     render::{mesh::Indices, render_resource::PrimitiveTopology},
 };
+use bevy_common_assets::json::JsonAssetPlugin;
 use terra::{
     HasHexPosition, HexDirection, Point2, RenderConfig, Tile, TilePoint,
     VertexDirection, World, WorldConfig, WorldRenderer,
@@ -15,32 +17,39 @@ pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.insert_resource(WorldConfig {
-            radius: 100,
-            seed: 238758723847892,
-            ..default()
-        })
-        .insert_resource(
-            WorldRenderer::new(RenderConfig {
-                vertical_scale: 1.0,
-                ..default()
-            })
-            .unwrap(),
-        )
-        .add_startup_system(setup)
-        .add_startup_system(generate_world);
+        app.add_plugin(JsonAssetPlugin::<WorldConfig>::new(&["terra.json"]))
+            .insert_resource(
+                WorldRenderer::new(RenderConfig {
+                    vertical_scale: 1.0,
+                    ..default()
+                })
+                .unwrap(),
+            )
+            .add_startup_system(load_config)
+            .add_startup_system(init_scene)
+            .add_system(generate_world)
+            // Always delete *before* generating so we don't clobber new stuff
+            .add_system(delete_world.before(generate_world))
+            .add_system(render_world);
     }
 }
 
-#[derive(Bundle)]
-struct TileBundle {
-    tile: Tile,
-    #[bundle]
-    pbr_bundle: PbrBundle,
+#[derive(Resource)]
+struct WorldConfigHandle(Handle<WorldConfig>);
+
+fn load_config(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let config_handle =
+        WorldConfigHandle(asset_server.load("worlds/medium.terra.json"));
+    commands.insert_resource(config_handle);
 }
 
 /// Add static entities to the scene
-fn setup(mut commands: Commands) {
+fn init_scene(
+    renderer: Res<WorldRenderer>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     // Directional light emulates the sun
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -53,44 +62,6 @@ fn setup(mut commands: Commands) {
             .looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-}
-
-fn generate_world(
-    mut commands: Commands,
-    world_config: Res<WorldConfig>,
-    renderer: Res<WorldRenderer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Generate the world
-    info!("Generating world");
-    let world = World::generate(world_config.to_owned()).unwrap();
-
-    // Spawn all the world tiles
-    debug!("Spawning tile meshes");
-    let tile_mesh_handle = meshes.add(tile_mesh(&renderer));
-    for tile in world.into_tiles().into_values() {
-        let position_2d = renderer.hex_to_screen_space(tile.position());
-        let height = renderer.tile_height(&tile) as f32;
-        let transform = Transform::from_xyz(
-            position_2d.x as f32,
-            0.0,
-            position_2d.y as f32,
-        )
-        .with_scale([1.0, height, 1.0].into());
-        let color = renderer.tile_color(&tile);
-
-        commands.spawn(TileBundle {
-            tile,
-            pbr_bundle: PbrBundle {
-                mesh: tile_mesh_handle.clone(),
-                material: materials
-                    .add(Color::rgb(color.red, color.green, color.blue).into()),
-                transform,
-                ..default()
-            },
-        });
-    }
 
     // Shitty ocean plane
     commands.spawn(PbrBundle {
@@ -103,6 +74,83 @@ fn generate_world(
         ),
         ..default()
     });
+}
+
+/// Generate a world and add each tile as its own entity. This just creates the
+/// underlying world data, nothing visual.
+fn generate_world(
+    mut commands: Commands,
+    config_assets: Res<Assets<WorldConfig>>,
+    mut asset_events: EventReader<AssetEvent<WorldConfig>>,
+) {
+    // Generate the world
+    for event in asset_events.iter() {
+        // On first config load, or whenever it's changed, generate a new world
+        if let AssetEvent::Created { handle }
+        | AssetEvent::Modified { handle } = event
+        {
+            // We know the asset is loaded by now
+            let world_config = config_assets.get(handle).unwrap();
+
+            info!("Generating world");
+            let world = World::generate(world_config.to_owned()).unwrap();
+
+            // Spawn each tile as a separate entity
+            for tile in world.into_tiles().into_values() {
+                commands.spawn(tile);
+            }
+        }
+    }
+}
+
+/// Delete the world whenever the config asset changes
+fn delete_world(
+    mut commands: Commands,
+    tile_query: Query<Entity, With<Tile>>,
+    mut asset_events: EventReader<AssetEvent<WorldConfig>>,
+) {
+    for event in asset_events.iter() {
+        dbg!(event);
+        if let AssetEvent::Modified { .. } = event {
+            info!("Deleting old world");
+            for entity in tile_query.iter() {
+                commands.entity(entity).despawn();
+            }
+        }
+    }
+}
+
+/// Generate visual meshes and other components for each tile in the world.
+/// Run whenever tiles are added to the world.
+fn render_world(
+    mut commands: Commands,
+    tile_query: Query<(Entity, &Tile), Added<Tile>>,
+    renderer: Res<WorldRenderer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // For each tile entity, we'll attach additional visual components
+    let tile_mesh_handle = meshes.add(tile_mesh(&renderer));
+    for (entity, tile) in tile_query.iter() {
+        let position_2d = renderer.hex_to_screen_space(tile.position());
+        let height = renderer.tile_height(tile) as f32;
+        let transform = Transform::from_xyz(
+            position_2d.x as f32,
+            0.0,
+            position_2d.y as f32,
+        )
+        .with_scale([1.0, height, 1.0].into());
+        let color = renderer.tile_color(tile);
+
+        // Attach a mesh to the existing tile entity
+        commands.entity(entity).insert(PbrBundle {
+            mesh: tile_mesh_handle.clone(),
+            material: materials
+                .add(Color::rgb(color.red, color.green, color.blue).into()),
+            transform,
+            ..default()
+        });
+    }
 }
 
 /// Build a 3d mesh of a hexagonal prism, representing a tile.
