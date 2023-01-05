3a1c988797f8363956764ac5d2f3694ed6d3ff3a
Lucas Pickering <lucas@lucaspickering.me>
2023-01-05 06:34:02 -0500

    Add RenderWorldEvent to unify logic of when to render

--- crates/3d/src/ui/render.rs
+++ crates/3d/src/ui/render.rs
@@ -1,5 +1,5 @@
-use crate::ui::enum_radio_select;
-use bevy::prelude::ResMut;
+use crate::{ui::enum_radio_select, world::event::RenderWorldEvent};
+use bevy::prelude::{DetectChanges, EventWriter, ResMut};
 use bevy_egui::{egui::Window, EguiContext};
 use terra::{RenderConfig, TileLens};
 
@@ -7,6 +7,7 @@ use terra::{RenderConfig, TileLens};
 pub fn render_config_ui(
     mut egui_context: ResMut<EguiContext>,
     mut render_config: ResMut<RenderConfig>,
+    mut render_world_events: EventWriter<RenderWorldEvent>,
 ) {
     Window::new("Render Config").show(egui_context.ctx_mut(), |ui| {
         ui.label("Lens");
@@ -22,11 +23,16 @@ pub fn render_config_ui(
             ]
             .into_iter(),
         ));
-        // Defer updating the render config until we know something actuall
+        // Defer updating the render config until we know something actually
         // changed. Otherwise we'll trigger a "change" in the render config
         // on every frame
         if lens != render_config.tile_lens {
             render_config.tile_lens = lens;
         }
     });
+
+    // If we changed the config at all, then trigger a new render
+    if render_config.is_changed() {
+        render_world_events.send(RenderWorldEvent);
+    }
 }
--- crates/3d/src/world/event.rs
+++ crates/3d/src/world/event.rs
@@ -1,2 +1,5 @@
 /// Triggered to *start* a new world generation
 pub struct GenerateWorldEvent;
+
+/// Triggered to *start* rendering a world, after it has been generated
+pub struct RenderWorldEvent;
